349e750991135d0c23674cc0ba67905538ad6ac8
Fabiola Rivera <fabiolarivera@ug.uchile.cl>
2018-10-24 21:59:56 -0300

    Creation of the abstract class AgeOfEmpireObjects Creation of getAttack and getHeal methods in the AgeOfEmpireObjects class Edition of the Unit and Buildings classes (Implementation of legacy methods of the AgeOfEmpireObject class and cleaning of repeated code) Complete implementation for the Castle class of the different methods inherited from the Building class Correction of Barracks class Removed the attackedByBarrack method in all classes that implement Attackable

--- src/CC3002/T1/Castle.java
+++ src/CC3002/T1/Castle.java
@@ -1,4 +1,42 @@
 package CC3002.T1;
 
-public class Castle{
+public class Castle extends Building implements Attacker{
+
+    @Override
+    public void attack(Attackable something) {
+        if (this.isAlive()){
+            something.attackedByCastle(this);
+        }
+    }
+
+    @Override
+    public float getAttackPoints() {
+        return 0;
+    }
+
+    //Attacked by other Buildings
+    public void attackedByCastle(Castle fort){
+        this.getHit(fort.getAttackPoints(),(float)0.1);
+    }
+
+    //Attacked by Units
+    public void attackedByInfantryUnit(InfantryUnit soldier){
+        this.getHit(soldier.getAttackPoints(), (float) 0.3);
+    }
+    public void attackedByArcherUnit(ArcherUnit bowman){
+        this.getHit(bowman.getAttackPoints(), (float) 0.1);
+    }
+    public void attackedByCavalry(CavalryUnit horseman){
+        this.getHit(horseman.getAttackPoints(), (float) 0.3);
+    }
+    public void attackedBySiegeUnit(SiegeUnit bigWeapon){
+        this.getHit(bigWeapon.getAttackPoints(),(float) 2.0);
+    }
+    public void attackedByMonk(Monk priest){
+        ;
+    }
+    public void attackedByVillager(Villager peasant) {
+        this.getHeal(peasant.getAttackPoints(),(float)0.3);
+    }
+
 }
